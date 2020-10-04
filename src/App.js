6d2fda543634b279001960dcdd839b41b335ba99
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-04 09:57:43 +0530

    Added RollDice component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 import './App.css'
-import Die from "./Die";
+import RollDice from "./RollDice";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Die face="five" />
+        <RollDice />
       </div>
     );
   }
--- src/Die.js
+++ src/Die.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
+import './Die.css'
 
 class Die extends Component {
-  render() {
-    return (
-      <i className={`fas fa-dice-${this.props.face}`} />
-    );
-  }
+    render() {
+        return (
+            <i className={`Die fas fa-dice-${this.props.face}`} />
+        );
+    }
 }
 
 export default Die;
